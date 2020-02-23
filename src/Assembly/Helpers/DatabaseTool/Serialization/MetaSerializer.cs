99806eb05472d2ff0976d5cf79ffd341ebbb2b85
MoMods <m54motorsports@gmail.com>
2020-02-23 12:35:17 -0500

    Add MetaSerializer class
    Add new MetaSerializer class to handle serializer options.

---
+++ src/Assembly/Helpers/DatabaseTool/Serialization/MetaSerializer.cs
@@ -0,0 +1,28 @@
+﻿using Assembly.Metro.Controls.PageTemplates.Games.Components.MetaData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Assembly.Helpers.Models
+{
+    class MetaSerializer
+    {
+        private readonly IList<MetaField> _metaStorage;
+
+        public MetaSerializer(IList<MetaField> metaStorage)
+        {
+            _metaStorage = metaStorage;
+        }
+
+        public string JavaScriptMetaSerializer()
+        {
+            var serializer = new JavaScriptSerializer() { MaxJsonLength = 2147483644 };
+            var serializedResult = serializer.Serialize(_metaStorage);
+
+            return serializedResult;
+        }
+    }
+}
