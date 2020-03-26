90868e8ab01de129c59649c16e787672e8e3bef9
MoMods <m54motorsports@gmail.com>
2020-03-26 10:36:26 -0400

    Add IDatabaseConnector interface

---
+++ src/Assembly/Helpers/DatabaseTool/Models/IDatabaseConnector.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+
+namespace Assembly.Helpers.Database.Models
+{
+    public interface IDatabaseConnector
+    {
+        bool TestConnection();
+        void UploadToDatabase(string key, byte[] value, string keyModifier = null);
+        object DownloadFromDatabase(string key, string keyModifier = null);
+        void ClearDatabaseContents(string keyModifier);
+
+    }
+}
