b0099d0c09b798fd2835f1d0741a5718ff56af08
MoMods <m54motorsports@gmail.com>
2020-03-27 16:07:02 -0400

    Add DatabaseTool view

---
+++ src/Assembly/Helpers/DatabaseTool/Views/DatabaseTool.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Assembly.Helpers.Database.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assembly.Helpers.Database.Views
+{
+    /// <summary>
+    /// Interaction logic for DatabaseTool.xaml
+    /// </summary>
+    public partial class DatabaseTool
+    {
+        public DatabaseTool()
+        {
+            InitializeComponent();
+            DataContext = new DatabaseToolViewModel();
+        }
+
+        public DatabaseTool(string pathPath)
+        {
+            InitializeComponent();
+            DataContext = new DatabaseToolViewModel();
+
+            tabPanel.SelectedIndex = 1;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
